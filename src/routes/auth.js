9e0021e954fbcbe89167f88ff54b61e9cdd694fb
catalin <cdojamihai@gmail.com>
2023-10-27 10:51:47 +0200

    changed the auth behaviour to use pools and async

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { register, login, logout, justinfo } from "../controllers/auth.js";
+import { registerAsync, loginAsync, logout, justinfo } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.get("/info", justinfo)
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerAsync);
+router.post("/login", loginAsync);
 router.post("/logout", logout);
 
 export default router;
